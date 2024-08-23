5b855801800d8b6e7c1c93e7f3d8349d95b49d33
jekky25 <jekky25@list.ru>
2024-08-23 08:42:37 +0300

    Установка ларавел на опен сервер 6

--- composer.php
+++ composer.php
@@ -11,4 +11,8 @@ composer update -W
 composer-php8.1 install
 
 //Для хостинга бегет, новый композер от версии 2 и выше. При установке в папку и адпейте сайта с терминала из админки хостинга
-php8.1 ~/.local/bin/composer install//установить ларавел на open server 6 после файлы из папки laravel_dev переместить в корень
+php8.1 ~/.local/bin/composer install
+
+
+//установить ларавел на open server 6 после файлы из папки laravel_dev переместить в корень
+composer create-project --prefer-dist laravel/laravel laravel_dev

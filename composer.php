eeb703551ed443f8cf106b7eaa9762d328b25121
jekky25 <jekky25@list.ru>
2024-12-16 07:17:09 +0300

    Запуск artisan на хостинге beget

--- composer.php
+++ composer.php
@@ -13,6 +13,9 @@ composer-php8.1 install
 //Для хостинга бегет, новый композер от версии 2 и выше. При установке в папку и адпейте сайта с терминала из админки хостинга
 php8.1 ~/.local/bin/composer install
 
+//Для хостинга бегет, запуск артисан из терминала
+php8.2 artisan migrate
+
 
 //установить ларавел на open server 6 после файлы из папки laravel_dev переместить в корень
 composer create-project --prefer-dist laravel/laravel laravel_dev

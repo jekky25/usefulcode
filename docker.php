3c9deb5720499c96bb34e08303097ecaa9813c0f
jekky25 <jekky25@list.ru>
2025-05-30 21:23:41 +0300

    Команды для Docker и Ubuntu

--- docker.php
+++ docker.php
@@ -14,3 +14,28 @@ docker-compose down
 
 //ребилд докера после изменения параметров
 docker-compose build
+
+//запуск докера по названию контейнера
+docker run -d -p 80:80 docker/getting-started
+
+//список установленных убунт, где покаазана, какая из них используется по умолчанию
+wslconfig /list
+
+//установка нужной убунты по умолчанию
+/setdefault Ubuntu-18.04
+
+//перейти в папку убунты
+cd \\wsl.localhost\Ubuntu-22.04\home\userName\projects\test2\
+
+//зайти внутрь контейнера
+docker exec -it project_app bash
+
+//выйти из контейнера
+
+командой exit
+
+или клавиши
+Ctrl + D
+
+//меняем атрибуты файлов
+sudo chmod 777 -R ./

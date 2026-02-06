a497527f1f91616961f2efbfc7999b317a89beec
jekky25 <jekky25@list.ru>
2026-02-06 21:52:23 +0300

    Запуск json-server из консоли

--- node.js
+++ node.js
@@ -9,3 +9,7 @@ osp node install lts
 https://beget.com/ru/kb/how-to/web-apps/node-js?_gl=1*12ozc0y*FPAU*NjI0MjQyMDIuMTc0MzM1MDYzNQ..*_ga*MTQ1ODE1MDU0NC4xNzQzMzUwNjM0*_ga_PZJSKZPBP7*MTc0NjE4MjU5MC4zMS4xLjE3NDYxODI4ODQuMC4wLjA.*_fplc*NkNxN0F3aHNNVkpTJTJGTVhwa1dtJTJGRjhtRVcwcm9SUmE5JTJGQnJoaUhtTlBjb2gzYjk0ek9OR2s1M3hyZEluJTJCZ3hheGpqSEQwJTJGUHYwZSUyQmlWZ3QxMUxrQUlYNWh2JTJGcUhKYk9DOERjV2x5R2glMkZzZ2xWdnVKNnhqOEh0Z1BpUVlSUSUzRCUzRA..#podklyuchenie-k-akkauntu
 
 делается через терминал админпанели, в которой запускается докер контейнер. Потом работаем через него
+
+
+//запуск json server из консоли
+npx json-server --watch db.json --port 3001
